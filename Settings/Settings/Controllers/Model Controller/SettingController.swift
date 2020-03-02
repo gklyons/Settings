60cf1fb2cef530957b5007d45a4a39e1456891e7
gklyons <30358106+gklyons@users.noreply.github.com>
2020-03-02 11:07:55 -0700

    Model & Model Controller

---
+++ Settings/Settings/Controllers/Model Controller/SettingController.swift
@@ -0,0 +1,9 @@
+//
+//  SettingController.swift
+//  Settings
+//
+//  Created by Garrett Lyons on 3/2/20.
+//  Copyright © 2020 Karl Pfister. All rights reserved.
+//
+
+import Foundation
---
+++ Settings/Settings/Model/Setting.swift
@@ -0,0 +1,9 @@
+//
+//  Setting.swift
+//  Settings
+//
+//  Created by Garrett Lyons on 3/2/20.
+//  Copyright © 2020 Karl Pfister. All rights reserved.
+//
+
+import Foundation
