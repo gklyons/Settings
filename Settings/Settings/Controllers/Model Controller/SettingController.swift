4719b0ac5857d651e72f737e0644a89026b42821
gklyons <30358106+gklyons@users.noreply.github.com>
2020-03-02 11:26:23 -0700

    Setting Controller Method

--- Settings/Settings/Controllers/Model Controller/SettingController.swift
+++ Settings/Settings/Controllers/Model Controller/SettingController.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+class SettingController {
+    
+    // MARK: - Source of Truth
+    let settings: [Setting] = {
+        let music = Setting(settingTitle: "Music", icon: #imageLiteral(resourceName: "iTunes"), isOn: false)
+        let appStore = Setting(settingTitle: "App Store", icon: #imageLiteral(resourceName: "appStore"), isOn: false)
+        let iBooks = Setting(settingTitle: "Book", icon: #imageLiteral(resourceName: "iBooks"), isOn: false)
+        return[music, appStore, iBooks]
+    }()
+    
+    // MARK: - Methods
+    func toggleIsOn(for setting: Setting) {
+        
+    }
+}
--- Settings/Settings/Model/Setting.swift
+++ Settings/Settings/Model/Setting.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2020 Karl Pfister. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+/**
+ Creates our Setting Object - *MODEL*
+ - Properties:
+    - name: The 'String'  identifier for the Setting
+    - icon: the 'UIImage' that matches the Setting
+    - isOn: The 'Bool' to designate whether the Setting is on or not
+ */
+
+class Setting {
+    let settingTitle: String
+    let icon: UIImage
+    var isOn: Bool
+    
+    init(settingTitle: String, icon: UIImage, isOn: Bool) {
+        self.settingTitle = settingTitle
+        self.icon = icon
+        self.isOn = isOn
+    }
+}
